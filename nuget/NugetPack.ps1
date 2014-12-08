cef07d5c5a7fe0022818344e92d675e77ee41d0c
George Hahn <george@genericmaker.com>
2014-12-08 00:53:39 -0500

    Push the nupkg myself because appveyor won't

--- nuget/NugetPack.ps1
+++ nuget/NugetPack.ps1
@@ -1,4 +1,7 @@
 $proj = "Charlotte"
+$nugetServer = $env:NugetServer
+$apiKey = $env:NugetKey
+
 $root = $env:APPVEYOR_BUILD_FOLDER
 $file = "$root\$($proj)\bin\$($env:CONFIGURATION)\$($proj).dll"
 $versionStr = "$($env:APPVEYOR_BUILD_VERSION)"
@@ -10,11 +13,12 @@ $content = $content -replace '\$file\$',$file
 
 $content | Out-File $root\nuget\$proj.compiled.nuspec
 
-& nuget pack $root\nuget\$proj.compiled.nuspec -OutputDirectory $root\nuget\
+& nuget pack $root\nuget\$proj.compiled.nuspec -OutputDirectory $root\nuget\ -Version $versionStr
 
 If($lastexitcode -eq 0)
 {
 	Write-Host "Nuget package built successfully"
+	nuget push nuget\*.nupkg -Source $nugetServer $apiKey
 } else {
 	Write-Host "Nuget packaging error $($lastexitcode)"
 }
